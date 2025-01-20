1d1950f845dce1be1c548be89b36b0bfc44ed591
ryanlin02 <126218351+ryanlin02@users.noreply.github.com>
2025-01-20 23:30:59 +0800

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// firebase-config.js
+
+// 從 Firebase Console 複製的設定
+const firebaseConfig = {
+    apiKey: "你的-api-key",
+    authDomain: "你的-project-id.firebaseapp.com",
+    projectId: "你的-project-id",
+    storageBucket: "你的-project-id.appspot.com",
+    messagingSenderId: "你的-messaging-sender-id",
+    appId: "你的-app-id"
+};
+
+// 導出設定
+export default firebaseConfig;
